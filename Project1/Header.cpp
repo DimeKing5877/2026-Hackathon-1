dbcfef42a993383b1424608e021036c73bae69df
DerrekDC <derrek22school@gmail.com>
2026-02-21 12:44:43 -0800

    basics

---
+++ Project1/Header.cpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <iostream>
+#include <time.h>
+#inclide <stdlib.h>
+
+
+
+class LinkList {
+public:
+
+
+
+
+private:
+	
+
+
+	//functions
+	//load the list
+	//save the list
+	//delete item
+	//add new item
+	//
+
+
+
+};
+
+
+class File {
+public:
+
+private:
+
+};
+
+
+class userInterface {
+
+
+
+
+};
+
+
+
+
+class keycreation {
+
+
+
+};
