4dd4fd36a46d1602dd03203d56681a8eb6e1c54e
DimeKing5877 <dennis55carpenter589@gmail.com>
2026-02-21 19:32:34 -0800

    userkey
    prompts the user to enter there key if none then a key is generated

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,11 +1,19 @@
 #include "Header.hpp"
-
+#include "UIHeader.hpp"
 
 
 
 int main(void) {
+	UImain();
 
 
-
-	return 1;
+	return 0;
+}
+void UImain() {
+	std::string filename = "encripted.txt";
+	//checks if the user has a key or needs to generat one
+	std::string userKey = userKeyEntry(filename);
+	//pritn user key
+	std::cout << "User key: " << userKey << std::endl;
+	system("pause");
 }
